0cda5d85eefc0aeb1f167436c1686ee0622bbba7
vsisquesc-RD <victor.sisques@rightdecisions.es>
2025-08-02 17:13:56 +0200

    falta key bindings

---
+++ controller.py
@@ -0,0 +1,65 @@
+from signal import pause
+import time
+
+import libs.config
+import libs.uinput
+
+
+# configuración
+buttons = libs.config.buttons
+outputs = libs.config.outputs
+all_chars = libs.config.all_chars
+device = libs.uinput.build_device(all_chars)
+        
+# globales
+LAST_EXEC_TIME = 0
+MASK = 0
+REPEAT_DELAY = libs.config.repeat_delay
+
+def check_btns():
+    global LAST_EXEC_TIME
+    global MASK
+    global REPEAT_DELAY
+    now = time.monotonic()
+
+    # Ignora si se ejecutó muy recientemente
+    if now - LAST_EXEC_TIME < REPEAT_DELAY:
+        return
+    
+    MASK = (buttons['driver_up'].is_pressed )     | \
+           (buttons['driver_down'].is_pressed) << 1 | \
+           (buttons['passenger_up'].is_pressed) << 2 | \
+           (buttons['passenger_down'].is_pressed) << 3
+
+        
+def dispatchFunction(held = False):
+    global MASK
+    global LAST_EXEC_TIME
+    
+    now = time.monotonic()
+    currMask = held << 4 | MASK
+    if currMask in outputs and outputs[currMask]!= None:
+        key = outputs[currMask]
+        libs.uinput.type_char(device, key)
+        LAST_EXEC_TIME = now
+        MASK = 0
+        
+
+
+buttons['driver_up'].when_pressed = check_btns
+buttons['driver_down'].when_pressed = check_btns
+buttons['passenger_up'].when_pressed = check_btns
+buttons['passenger_down'].when_pressed = check_btns
+
+buttons['driver_up'].when_released = dispatchFunction
+buttons['driver_down'].when_released = dispatchFunction
+buttons['passenger_up'].when_released = dispatchFunction
+buttons['passenger_down'].when_released = dispatchFunction
+
+buttons['driver_up'].when_held = lambda : dispatchFunction(True)
+buttons['driver_down'].when_held = lambda : dispatchFunction(True)
+buttons['passenger_up'].when_held = lambda : dispatchFunction(True)
+buttons['passenger_down'].when_held = lambda : dispatchFunction(True)
+
+pause()
+
---
+++ libs/uinput.py
@@ -0,0 +1,33 @@
+import uinput
+
+
+def get_key_code(char):
+    key_name = f"KEY_{char.upper()}"
+    return getattr(uinput, key_name, None)
+
+def type_char(device, char):
+    key = get_key_code(char)
+    if not key:
+        return
+
+    if char.isupper():
+        device.emit(uinput.KEY_LEFTSHIFT, 1)
+        device.emit_click(key)
+        device.emit(uinput.KEY_LEFTSHIFT, 0)
+    else:
+        device.emit_click(key)
+
+ 
+ 
+def build_device(chars):
+    # Construir lista de códigos necesarios
+    uinput_keys = [uinput.KEY_LEFTSHIFT]  # por si hay mayúsculas
+    for char in chars:
+        key_code = get_key_code(char)
+        if key_code:
+            uinput_keys.append(key_code)
+        else:
+            print(f"Advertencia: tecla '{char}' no reconocida por uinput")
+    # Crear el dispositivo con las teclas necesarias
+    device = uinput.Device(uinput_keys)  
+    return device
